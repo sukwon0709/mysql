8be6fa3cb2d9a4b2f26d577f782248f9a19dd354
Sukwon Oh <sukwon0709@gmail.com>
2016-09-09 19:46:19 -0400

    Added more tests

--- tests/ParserTest.hs
+++ tests/ParserTest.hs
@@ -11,7 +11,7 @@ import qualified Token            as Tok
 
 
 testCases :: [TestTree]
-testCases = [ts1, ts2, ts3, ts4, ts5]
+testCases = [ts1, ts2, ts3, ts4, ts5, ts6, ts7, ts8, ts9, ts10, ts11]
 
 -- ts1 :: TestTree
 -- ts1 = testCase "Create Table1" $ parseTest createTableStmt $
@@ -146,3 +146,191 @@ ts5 = testCase "Expression4" $
             (Syn.Lit
               (Syn.NLit "6")))))))
 
+ts6 :: TestTree
+ts6 = testCase "Check Expression1" $
+  (parse checkExpr ""
+    (Lex.alexScanTokens $ "CHECK(NOT(1 <= CourseID and CourseID <= 10) or Points < 6)"))
+  @?= Right
+  Syn.CheckExpr {
+      Syn.checkExpr =
+          (Syn.EOr
+            (Syn.ENot
+              (Syn.BooleanPrimary
+                (Syn.Predicate
+                  (Syn.BitExpr
+                    (Syn.SimpleExpr
+                      (Syn.SEList
+                        [Syn.EAnd
+                          (Syn.BooleanPrimary
+                            (Syn.BPLTE
+                              (Syn.Predicate (Syn.BitExpr (Syn.SimpleExpr (Syn.Lit (Syn.NLit "1")))))
+                              (Syn.BitExpr (Syn.SimpleExpr (Syn.Ident "CourseID")))))
+                          (Syn.BooleanPrimary
+                            (Syn.BPLTE
+                              (Syn.Predicate (Syn.BitExpr (Syn.SimpleExpr (Syn.Ident "CourseID"))))
+                              (Syn.BitExpr (Syn.SimpleExpr (Syn.Lit (Syn.NLit "10"))))))
+                        ]))))))
+            (Syn.BooleanPrimary
+              (Syn.BPLT
+                (Syn.Predicate
+                  (Syn.BitExpr
+                    (Syn.SimpleExpr
+                      (Syn.Ident "Points"))))
+                (Syn.BitExpr
+                  (Syn.SimpleExpr
+                    (Syn.Lit
+                      (Syn.NLit "6")))))))
+      }
+
+ts7 :: TestTree
+ts7 = testCase "ColumnDef1" $
+  (parse createDef ""
+    (Lex.alexScanTokens $ "StudentID tinyint not null"))
+  @?= Right Syn.ColumnDef
+  {
+    Syn.name = "StudentID"
+  , Syn.definition = Syn.FieldDef
+                     {
+                       Syn.fieldType = Syn.DTypeTinyInt (Just 0)
+                     , Syn.nullable = False
+                     , Syn.autoIncrement = False
+                     , Syn.uniqueKey = False
+                     , Syn.primaryKey = False
+                     }
+  , Syn.colDefRefDef = Nothing
+  }
+
+ts8 :: TestTree
+ts8 = testCase "ColumnDef2" $
+  (parse createDef ""
+    (Lex.alexScanTokens $ "CourseID tinyint not null"))
+  @?= Right Syn.ColumnDef
+  {
+    Syn.name = "CourseID"
+  , Syn.definition = Syn.FieldDef
+                     {
+                       Syn.fieldType = Syn.DTypeTinyInt (Just 0)
+                     , Syn.nullable = False
+                     , Syn.autoIncrement = False
+                     , Syn.uniqueKey = False
+                     , Syn.primaryKey = False
+                     }
+  , Syn.colDefRefDef = Nothing
+  }
+
+ts9 :: TestTree
+ts9 = testCase "ColumnDef3" $
+  (parse createDef ""
+    (Lex.alexScanTokens $ "Points tinyint not null"))
+  @?= Right Syn.ColumnDef
+  {
+    Syn.name = "Points"
+  , Syn.definition = Syn.FieldDef
+                     {
+                       Syn.fieldType = Syn.DTypeTinyInt (Just 0)
+                     , Syn.nullable = False
+                     , Syn.autoIncrement = False
+                     , Syn.uniqueKey = False
+                     , Syn.primaryKey = False
+                     }
+  , Syn.colDefRefDef = Nothing
+  }
+
+ts10 :: TestTree
+ts10 = testCase "PK1" $
+  (parse createDef ""
+    (Lex.alexScanTokens $ "PRIMARY KEY (StudentID, CourseID)"))
+  @?= Right Syn.PKDef
+  {
+    Syn.pkColNames = ["StudentID", "CourseID"]
+  }
+
+ts11 :: TestTree
+ts11 = testCase "Create Table2" $
+  (parse createTableStmt ""
+    (Lex.alexScanTokens $ "CREATE TABLE Scores \
+                          \(StudentID tinyint not null, \
+                          \ CourseID tinyint not null, \
+                          \ Points tinyint not null, \
+                          \ PRIMARY KEY (StudentID, CourseID), \
+                          \ CHECK(NOT(1 <= CourseID and CourseID <= 10) or Points < 6))"))
+  @?= Right Syn.CreateTableStmt
+  {
+    Syn.isTemporary = False
+  , Syn.tblName = "Scores"
+  , Syn.createDefinitions = [
+      Syn.ColumnDef
+      {
+        Syn.name = "StudentID"
+      , Syn.definition = Syn.FieldDef
+                         {
+                           Syn.fieldType = Syn.DTypeTinyInt (Just 0)
+                         , Syn.nullable = False
+                         , Syn.autoIncrement = False
+                         , Syn.uniqueKey = False
+                         , Syn.primaryKey = False
+                         }
+      , Syn.colDefRefDef = Nothing
+      },
+      Syn.ColumnDef
+      {
+        Syn.name = "CourseID"
+      , Syn.definition = Syn.FieldDef
+                         {
+                           Syn.fieldType = Syn.DTypeTinyInt (Just 0)
+                         , Syn.nullable = False
+                         , Syn.autoIncrement = False
+                         , Syn.uniqueKey = False
+                         , Syn.primaryKey = False
+                         }
+      , Syn.colDefRefDef = Nothing
+      },
+      Syn.ColumnDef
+      {
+        Syn.name = "Points"
+      , Syn.definition = Syn.FieldDef
+                         {
+                           Syn.fieldType = Syn.DTypeTinyInt (Just 0)
+                         , Syn.nullable = False
+                         , Syn.autoIncrement = False
+                         , Syn.uniqueKey = False
+                         , Syn.primaryKey = False
+                         }
+      , Syn.colDefRefDef = Nothing
+      },
+      Syn.PKDef
+      {
+        Syn.pkColNames = ["StudentID", "CourseID"]
+      },
+      Syn.CheckExpr {
+          Syn.checkExpr =
+              (Syn.EOr
+                (Syn.ENot
+                  (Syn.BooleanPrimary
+                    (Syn.Predicate
+                      (Syn.BitExpr
+                        (Syn.SimpleExpr
+                          (Syn.SEList
+                            [Syn.EAnd
+                              (Syn.BooleanPrimary
+                                (Syn.BPLTE
+                                  (Syn.Predicate (Syn.BitExpr (Syn.SimpleExpr (Syn.Lit (Syn.NLit "1")))))
+                                  (Syn.BitExpr (Syn.SimpleExpr (Syn.Ident "CourseID")))))
+                              (Syn.BooleanPrimary
+                                (Syn.BPLTE
+                                  (Syn.Predicate (Syn.BitExpr (Syn.SimpleExpr (Syn.Ident "CourseID"))))
+                                  (Syn.BitExpr (Syn.SimpleExpr (Syn.Lit (Syn.NLit "10"))))))
+                            ]))))))
+                (Syn.BooleanPrimary
+                  (Syn.BPLT
+                    (Syn.Predicate
+                      (Syn.BitExpr
+                        (Syn.SimpleExpr
+                          (Syn.Ident "Points"))))
+                    (Syn.BitExpr
+                      (Syn.SimpleExpr
+                        (Syn.Lit
+                          (Syn.NLit "6")))))))
+          }
+      ]
+  }
